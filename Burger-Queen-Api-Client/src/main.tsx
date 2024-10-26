f4add78d3dfa29a2cde49b87c44871889e886a7b
Diana <educ.dana@gmail.com>
2024-10-26 18:55:02 -0500

    chore: rename styles

--- Burger-Queen-Api-Client/src/main.tsx
+++ Burger-Queen-Api-Client/src/main.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import './Styles/login.css'
-import './Styles/products.css'
+import './Styles/totalProducts.css'
 import './Styles/ordersList.css'
 import App from "./App.tsx";
 
